7e7da549f52caa2783ffd8253771103f7d5ca611
cianclarke <cian@cianclarke.com>
2014-02-12 18:22:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# fh-connector-sap-netweaver
---
+++ cloud/main.js
@@ -0,0 +1,24 @@
+
+/*
+ Connects to a SAP Netweaver instance - returns the SAP version number on success
+ NB - SAP Netweaver support is enabled on a per-customer basis
+ */
+exports.netweaver = function(){
+ var conParams = {
+   ashost: process.env.SAP_HOST,
+   sysid: process.env.SAP_SYSID,
+   sysnr: process.env.SAP_SYSNR,
+   user: process.env.SAP_USER,
+   passwd: process.env.SAP_PASSWORD,
+   client: process.env.SAP_CLIENT
+ };
+
+ var con = new sapnwrfc.Connection;
+
+ con.Open(conParams, function(err) {
+   if (err) {
+     return cb(err);
+   }
+   return cb(null, con.GetVersion());
+ });
+};
