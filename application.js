360f600feaa53bf9f2634c1b2714d87ad525d27e
Martin Murphy <martin.murphy@feedhenry.com>
2014-10-06 16:22:05 +0100

    update application.js for fhlint, add static file server, include default index.html - #8091

--- application.js
+++ application.js
@@ -2,7 +2,12 @@ var mbaasApi = require('fh-mbaas-api');
 var express = require('express');
 var mbaasExpress = mbaasApi.mbaasExpress();
 var cors = require('cors');
-var mainjs = require('./main.js');
+
+// list the endpoints which you want to make securable here
+var securableEndpoints;
+// fhlint-begin: securable-endpoints
+securableEndpoints = [];
+// fhlint-end
 
 var app = express();
 
@@ -10,23 +15,23 @@ var app = express();
 app.use(cors());
 
 // Note: the order which we add middleware to Express here is important!
-app.use('/sys', mbaasExpress.sys([]));
+app.use('/sys', mbaasExpress.sys(securableEndpoints));
 app.use('/mbaas', mbaasExpress.mbaas);
 
 // Note: important that this is added just before your own Routes
 app.use(mbaasExpress.fhmiddleware());
 
-app.use('/cloud', mbaasExpress.cloud(mainjs));
+// allow serving of static files from the public directory
+app.use(express.static(__dirname + '/public'));
 
-// You can define custom URL handlers here, like this one:
-app.use('/', function(req, res) {
-  res.end('Your Cloud App is Running');
-});
+// fhlint-begin: custom-routes
+app.use('/cloud', mbaasExpress.cloud(require('./main.js')));
+// fhlint-end
 
 // Important that this is last!
 app.use(mbaasExpress.errorHandler());
 
 var port = process.env.FH_PORT || process.env.VCAP_APP_PORT || 8001;
-module.exports = app.listen(port, function() {
+app.listen(port, function() {
   console.log("App started at: " + new Date() + " on port: " + port);
 });
