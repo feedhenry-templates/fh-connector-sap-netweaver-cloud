e444b0e072e4708a5431e807762043213d27fd87
cianclarke <cian@cianclarke.com>
2015-02-26 13:55:37 -0500

    127.0.0.1 -> 0.0.0.0

--- application.js
+++ application.js
@@ -32,7 +32,7 @@ app.use('/cloud', mbaasExpress.cloud(require('./main.js')));
 app.use(mbaasExpress.errorHandler());
 
 var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
-var host = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var server = app.listen(port, host, function() {
   console.log("App started at: " + new Date() + " on port: " + port); 
 });
